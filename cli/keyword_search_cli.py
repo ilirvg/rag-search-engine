aabc0f363db90b938035da3ff53d84dd65c2d36a
Ilir Gruda <ilir_gruda@hotmail.com>
2025-11-29 08:24:29 +0100

    keyword search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python3
-
 import argparse
-
+import json
+from pathlib import Path
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -14,12 +13,40 @@ def main() -> None:
 
     match args.command:
         case "search":
-            # print the search query here
-            print("Searching for: " + args.query)
+            search_query = args.query
+            searched_movies = search_comand(search_query)
+
+            print(f'Searching for: {search_query}')
+            for index, movie in enumerate(searched_movies[:5]):
+                print(f'{index + 1}. {movie['title']}')
+
             pass
         case _:
             parser.print_help()
 
 
+
+def get_movies_list() -> list[dict]:
+    # Get the directory where this script is located and go up one level from cli/ to repo root
+    script_dir = Path(__file__).parent.parent
+    data_path = script_dir / 'data' / 'movies.json'
+
+    try:
+        with open(data_path, 'r') as data_file:
+            data = json.load(data_file)
+            return data['movies']
+    except Exception as e:
+        print(f'Failed to read the movies data: {e}')
+        return
+
+def search_comand(query:str) -> list[dict]:
+    movies = []
+    for movie in get_movies_list():
+        if query.lower() in movie['title'].lower():
+            movies.append(movie)
+
+    movies.sort(key=lambda item: item['id'])
+    return movies
+
 if __name__ == "__main__":
-    main()
+    main()  
